6d1b0076861138721076c495358014b7ac52ceb1
Alex Riasanovsky <a.riasanovsky@gmail.com>
2024-03-29 17:55:44 -0400

    im like so excited, I can't wait to see pretty numbers on my screen omg

--- src/ls_sct.rs
+++ src/ls_sct.rs
@@ -5,13 +5,15 @@ use crate::{inplace_sct_signed::SignedCut, sct_helper::Sct};
 pub struct CutSetLdl {
     // `nrows * ncols`, the flat dimension of `H`, our space of tensors
     s: f64,
-    // `- s * M^T` where `M = L^{-1}` and `(X^TX) = s * LL^T`
+    // `- s * M^T` where `M = L^{-1}` and `X^TX = s * LL^T` (ldl w/ `D = s * I`)
     m_t: Mat<f64>,
 }
 
 impl CutSetLdl {
     pub fn new(nrows: usize, ncols: usize, rank: usize) -> Self {
-        todo!()
+        let s = (nrows * ncols) as f64;
+        let m_t = Mat::with_capacity(rank, rank);
+        Self { s, m_t }
     }
 
     pub fn add_column(&mut self, sct: &Sct) -> f64 {
@@ -20,12 +22,20 @@ impl CutSetLdl {
 }
 
 pub struct CutSetBuffers {
-    
+    s_signs: Col<f64>,
+    s_image: Col<f64>,
+    t_signs: Col<f64>,
+    t_image: Col<f64>,
 }
 
 impl CutSetBuffers {
     pub fn new(nrows: usize, ncols: usize) -> Self {
-        todo!()
+        Self {
+            s_signs: Col::zeros(nrows),
+            s_image: Col::zeros(ncols),
+            t_signs: Col::zeros(ncols),
+            t_image: Col::zeros(nrows),
+        }
     }
 
     pub fn write_cut(
