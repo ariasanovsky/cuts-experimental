159214f7aa528ed8a7e136fe155a437d46ea0d58
Alex Riasanovsky <a.riasanovsky@gmail.com>
2024-04-14 07:31:14 -0400

    crashing on the sparse cut step

--- src/sparse_cut/helpers.rs
+++ src/sparse_cut/helpers.rs
@@ -8,17 +8,29 @@ pub struct SparseParameters {
 
 impl core::fmt::Display for SparseParameters {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        write!(f, "iters.{}.bits.{}", self.max_iters, self.max_bits)
     }
 }
 
 pub struct SparseSct {
+    s: Vec<Vec<(usize, Sign)>>,
+    t: Vec<Vec<(usize, Sign)>>,
+    c: Vec<f64>,
+}
 
+#[derive(Clone, Copy, Debug)]
+pub enum Sign {
+    Positive,
+    Negative,
 }
 
 impl SparseSct {
     pub fn new(nrows: usize, ncols: usize, max_u16s: usize) -> Self {
-        todo!()
+        Self {
+            s: Vec::with_capacity(max_u16s),
+            t: Vec::with_capacity(max_u16s),
+            c: Vec::with_capacity(max_u16s),
+        }
     }
 
     pub fn extend_with(&mut self, s_signs: &Col<f64>, t_signs: &Col<f64>, cut: f64) {
